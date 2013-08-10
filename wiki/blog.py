4eaa01b00300cacced865c3da896069f679f15db
Michael Hackett <hackettma@gmail.com>
2013-08-10 15:47:22 -0500

    updates to main

--- wiki/blog.py
+++ wiki/blog.py
@@ -16,6 +16,16 @@ jinja_env = jinja2.Environment(
                                autoescape = True)
 
 
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+secret = 'fart'
+
+
 class BaseHandler(webapp2.RequestHandler):
   def write(self, *a, **kw):
     self.response.out.write(*a, **kw)
@@ -26,6 +36,71 @@ class BaseHandler(webapp2.RequestHandler):
 
   def render(self, template, **kw):
     self.write(self.render_str(template, **kw))  
+
+  def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
 
 
 class Wiki(db.Model):
@@ -41,8 +116,7 @@ class MainPage(BaseHandler):
 
   def get(self):
     wikis = db.GqlQuery("SELECT * FROM Wiki ORDER BY title ASC LIMIT 1")
-    user = {'user': "Michael"}
-    self.render_front(wikis=wikis, logged_in=True, user=user)
+    self.render_front(wikis=wikis)
 
 class Signup(BaseHandler):
   def render_front(self):
@@ -66,6 +140,7 @@ class Logout(BaseHandler):
 
 
   def get(self):
+    self.logout()
     self.render_front()
 
 class EditPage(BaseHandler):
